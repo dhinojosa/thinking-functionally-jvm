aedacc5bc67c8ebd91144ca9c655a12a287a3e91
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2026-06-07 09:37:48 -0600

    Added Java Project

---
+++ projects/thinking-functionally-java/src/test/java/com/thinkingfunctionally/SetupTest.java
@@ -0,0 +1,13 @@
+package com.thinkingfunctionally;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SetupTest {
+
+    @Test
+    void junitAndAssertJAreWired() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
